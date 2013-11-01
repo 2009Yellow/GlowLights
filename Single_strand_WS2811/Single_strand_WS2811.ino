ce855438719488707b5bdfd7ac7280d7b65fe7c0
unknown <tliu892@gmail.com>
2013-11-01 17:50:50 -0400

    Use for materials testing of LED strips with 3 wires, WS2811 chip

---
+++ Single_strand_WS2811/Single_strand_WS2811.ino
@@ -0,0 +1,28 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ400);
+
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  // Some example procedures showing how to display to the pixels:
+  //add for however many lights you want, colors are RGB
+  strip.setPixelColor(0, strip.Color(127, 0, 0)); 
+  strip.setPixelColor(1, strip.Color(0, 127, 0));
+  strip.setPixelColor(2, strip.Color(50,50,50));
+  strip.show();
+}
+
+
